e9b3a739869c114e9d64a69e771171be1516acdb
shermanleejm <leesherman@live.com.sg>
2021-08-27 16:13:41 +0800

    boilerplate for flask backend

---
+++ backend/app.py
@@ -0,0 +1,18 @@
+def CalculateCreditRisk():
+    ## use magic formula to calculate risk
+
+    ## write to db
+    pass
+
+def addTransaction():
+    pass
+
+def addRevenue():
+    pass
+
+def login():
+    pass
+
+def register():
+    pass
+
