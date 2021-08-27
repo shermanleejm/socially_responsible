82ec803d4b7a55f387fc070db3d1bc1bbd6fa46e
shermanleejm <leesherman@live.com.sg>
2021-08-27 17:07:06 +0800

    lol

--- backend/app.py
+++ backend/app.py
@@ -42,11 +42,13 @@ calc_credit_parser.add_argument(
 @api.doc(description="Calculates the risk profile score for a company.")
 class CalculateCreditRisk(Resource):
     @api.expect(calc_credit_parser)
-    def get(self, company):
+    def get(self):
         ## use magic formula to calculate risk
         ## Probability of Default = (loss given default * exposure at default) / expected loss (i.e., company revenue)
         ## write to db (conditional on how long it takes to calculate)
-        pass
+        company = calc_credit_parser.parse_args().get("company")
+        print(type(company))
+        return company, 200
 
 
 def addTransaction():
@@ -72,3 +74,7 @@ def newLoan():
 
 def checkLoanStatus():
     pass
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
