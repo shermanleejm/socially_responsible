7f23ea700480a127b68a9ce716635dc119f2e6a5
Yieh Yuheng <yiehyuheng@gmail.com>
2021-08-29 00:12:33 +0800

    Update db schema for investors to include salt

--- db_schema.sql
+++ db_schema.sql
@@ -33,4 +33,25 @@ CREATE TABLE IF NOT EXISTS `loan` (
 CREATE TABLE IF NOT EXISTS `investors` (
     `username` VARCHAR(256) NOT NULL PRIMARY KEY,
     `hashed_password` VARCHAR(1000) NOT NULL,
+    `salt` VARCHAR(1000) NOT NULL
 );
+
+INSERT INTO
+expenditure(amount,name,uen,timestamp)
+VALUES
+(500,"rent","testuen",'2021-05-28'),
+(600,"rent","testuen",'2021-05-29'),
+(700,"rent","testuen",'2021-05-30'),
+(100,"rent","testuen",'2021-05-31'),
+(400,"rent","testuen",'2021-06-01'),
+(300,"rent","testuen",'2021-06-02');
+
+INSERT INTO
+revenue(amount,name,uen,timestamp)
+VALUES
+(1000,"XRP","testuen",'2021-05-28'),
+(1000,"XRP","testuen",'2021-05-29'),
+(1200,"XRP","testuen",'2021-05-30'),
+(1300,"XRP","testuen",'2021-05-31'),
+(1500,"XRP","testuen",'2021-06-01'),
+(1900,"XRP","testuen",'2021-06-02');
